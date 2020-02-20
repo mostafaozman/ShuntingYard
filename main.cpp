073af6fde63d439bac9066656519b3c2a755c30c
mostafaozman <48702930+mostafaozman@users.noreply.github.com>
2020-02-19 21:10:23 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -17,6 +17,20 @@ struct Stack
     Stack *StackLink;
 };
 
+struct StackT
+{
+    tree* StackDataT;
+    StackT* StackLinkT;
+};
+
+tree* peekT();
+tree* popT();
+void StackPushT(tree* t);
+bool isStackEmptyT();
+StackT *topT = NULL;
+StackT *bottomT = NULL;
+
+
 bool isStackEmpty();
 void StackPush(char value);
 char pop();
@@ -36,7 +50,7 @@ struct Queue
 bool isQueueEmpty();
 void enqueue(int value);
 void dequeue();
-void showFirst();
+char showFirst();
 void printQueue();
 // queue pointers
 Queue *front = NULL;
@@ -48,12 +62,13 @@ void preFix(tree* n);
 void postFix(tree* n);
 void inFix(tree* n);
 tree* createData();
+tree* buildTree();
 
 int main()
 {
     //tree* a = new tree("a");
-    tree* data = createData() ;
-    inFix(data);
+    //tree* data = createData() ;
+    //inFix(data);
     
     
     int i = 0;
@@ -65,110 +80,103 @@ int main()
     
     cout << input << endl;
     
+    while(input[i] != '\0')
+    {
+        //cout << input[i];
+        if (input[i] == '(')
+        {
+            // add to stack
+            cout<<"\nstack push "<<input[i];
+            StackPush(input[i]);
+        }
+        // if it is an operand
+        else if (isalpha(input[i]))
+        {
+            cout<<"\nqueue "<<input[i];
+            enqueue(input[i]);
+        }
+        // If it is an operator
+        //check precedence with top of stack
+        //if greater then push into stack
+        //if not then pop all the stack into the queue
+        else if (input[i] == '+' || input[i] == '-' ||  input[i] == '*' || input[i] == '/' || input[i] == '^')
+        {
+            if (isStackEmpty())
+            {
+                cout<<"\nstack push "<<input[i];
+                StackPush(input[i]);
+            }
+            else if (findPrecedence(input[i]) >= findPrecedence(peek()))
+                StackPush(input[i]);
+            else
+            {
+                while(!isStackEmpty())
+                {
+                    enqueue(pop());
+                }
+                StackPush(input[i]);
+            }
+            
+        }
+        // if it closing bracket
+        else if (input[i] == ')')
+        {
+            while (peek() != '(')
+            {
+                enqueue(pop());
+            }
+            pop();
+        }
+        
+        //if ((isStackEmpty() == false) && (i == strlen(input)-1))
+        i++;
+        // if the stack is empty then add to queue
+    }
+    
+    //Now put everything in the queue
+    while(!isStackEmpty())
+        enqueue(pop());
+    
+    printQueue();
+    
+    tree* data = buildTree();
+    
     cout << "\nWould you like the output in infix, postfix, or prefix?" <<  endl;
     cin.get(input2, 50);
     cin.clear();
     cin.ignore(100000, '\n');
     cout <<  input2;
     
-    /*
-     
-     input[0] = '(';
-     input[1] = 'a';
-     input[2] = '+';
-     input[3] = 'b';
-     input[4] = ')';
-     input[5] = '\0';
-     */
-    
     if(strcmp(input2, "infix") == 0)
     {
-        cout << input;
+        inFix(data);
     }
     
-    else if (strcmp(input2, "postfix") == 0)
+    if(strcmp(input2, "postfix") == 0)
     {
-        while(input[i] != '\0')
-        {
-            //cout << input[i];
-            if (input[i] == '(')
-            {
-                // add to stack
-                cout<<"\nstack push "<<input[i];
-                StackPush(input[i]);
-            }
-            // if it is an operand
-            else if (isdigit(input[i]))
-            {
-                cout<<"\nqueue "<<input[i];
-                enqueue(input[i]);
-            }
-            // If it is an operator
-            //check precedence with top of stack
-            //if greater then push into stack
-            //if not then pop all the stack into the queue
-            else if (input[i] == '+' || input[i] == '-' ||  input[i] == '*' || input[i] == '/' || input[i] == '^')
-            {
-                if (isStackEmpty())
-                {
-                    cout<<"\nstack push "<<input[i];
-                    StackPush(input[i]);
-                }
-                else if (findPrecedence(input[i]) >= findPrecedence(peek()))
-                    StackPush(input[i]);
-                else
-                {
-                    while(!isStackEmpty())
-                    {
-                        enqueue(pop());
-                    }
-                    StackPush(input[i]);
-                }
-                
-                /*
-                 
-                 while (peek() == '+' || peek() == '-' || peek() == '*'|| peek() == '/' || peek() == '^')
-                 {
-                 if (findPrecedence(peek()) > findPrecedence(input[i]))
-                 {
-                 enqueue(pop());
-                 }
-                 else
-                 break;
-                 }*/
-            }
-            // if it closing bracket
-            else if (input[i] == ')')
-            {
-                while (peek() != '(')
-                {
-                    enqueue(pop());
-                }
-                pop();
-            }
-            
-            //if ((isStackEmpty() == false) && (i == strlen(input)-1))
-            i++;
-        }// if the stack is empty then add to queue
-        while(!isStackEmpty())
-            enqueue(pop());
-        
-        
-        printQueue();
-        
-        return 0;
-    }
-    else if(strcmp(input2, "prefix") == 0)
-    {
-        
+        postFix(data);
     }
     
+    if(strcmp(input2, "prefix") == 0)
+    {
+        preFix(data);
+    }
     else
     {
-        cout << "invalid command, check spelling" << endl;
+        cout << "Invalid Input." << endl;
     }
+    //Print the queue postfix
+    // printQueue();
+    
+    //Build the binary expression tree
+    //tree* data = buildTree() ;
+    
+    //Print the tree inFix
+    // inFix(data);
+    
 }
 
+//Checks if stack is empty
 bool isStackEmpty()
 {
     if (top == NULL)
@@ -178,6 +186,7 @@ bool isStackEmpty()
         return false;
     }
 }
+
 // adds to top  of the stack
 void StackPush(char value)
 {
@@ -186,6 +195,7 @@ void StackPush(char value)
     ptr -> StackLink = top;
     top = ptr;
 }
+
 // takes element  off the top of the stack
 char pop()
 {
@@ -223,6 +233,7 @@ char peek()
     
 }
 
+//Checks if queue is empty
 bool isQueueEmpty()
 {
     if (front == NULL && rear == NULL)
@@ -234,6 +245,7 @@ bool isQueueEmpty()
         return false;
     }
 }
+
 // add to queue
 void enqueue(int value)
 {
@@ -251,7 +263,9 @@ void enqueue(int value)
         rear -> QueueLink = ptr;
         rear = ptr;
     }
-} // delete from queue
+}
+
+// delete from queue
 void dequeue()
 {
     if (isQueueEmpty() == true)
@@ -272,18 +286,24 @@ void dequeue()
             free(ptr);
         }
     }
-} // show front of the queue
-void showFirst()
+}
+
+// show front of the queue
+char showFirst()
 {
     if (isQueueEmpty() == true)
     {
         cout << "The queue is empty." << endl;
+        return 0;
     }
     else
     {
         cout << front -> QueueData;
+        return front ->  QueueData;
     }
-} // prints queue
+}
+
+// prints queue
 void printQueue()
 {
     if (isQueueEmpty() == true)
@@ -302,6 +322,8 @@ void printQueue()
         cout<<"\n";
     }
 }
+
+//Purge stack
 void clearStack()
 {
     while (isStackEmpty() == false)
@@ -309,7 +331,8 @@ void clearStack()
         pop();
     }
 }
-// assigns precidence to each of the operators
+
+//Assigns opaerator precedence
 int findPrecedence(char op)
 {
     switch (op)
@@ -326,6 +349,7 @@ int findPrecedence(char op)
     return 0;
 }
 
+//Print Tree in Prefix
 void preFix(tree* n)
 {
     if (n == NULL)
@@ -340,6 +364,7 @@ void preFix(tree* n)
     }
 }
 
+//Print Tree in Postfix
 void postFix(tree* n)
 {
     if (n == NULL)
@@ -354,6 +379,7 @@ void postFix(tree* n)
     }
 }
 
+//Print tree inFix
 void inFix(tree* n)
 {
     if (n == NULL)
@@ -368,6 +394,74 @@ void inFix(tree* n)
     }
 }
 
+//Determines if tree stack is empty
+bool isStackEmptyT()
+{
+    if (topT == NULL)
+        return true;
+    else
+    {
+        return false;
+    }
+}
+
+//Pop tree stack
+tree* popT()
+{
+    tree* data = NULL;
+    if(isStackEmptyT() == true)
+    {
+        cout << "The stack is empty" << endl;
+        return data;
+    }
+    else
+    {
+        StackT *ptr = topT;
+        topT = ptr -> StackLinkT;
+        data = ptr -> StackDataT;
+        delete ptr;
+        return data;
+    }
+}
+
+//Push in tree stack
+void StackPushT(tree* t)
+{
+    StackT *ptr = new StackT();
+    ptr -> StackDataT = t;
+    ptr -> StackLinkT = topT;
+    topT = ptr;
+}
+/*void StackPushT(tree* t)
+ {
+ tree* ptr;
+ ptr -> getData() = t;
+ ptr -> getNext() = topT;
+ topT = ptr;
+ }*/
+
+
+tree* peekT()
+{
+    tree* op = NULL; //= new tree(" ");
+    if (isStackEmptyT() == true)
+    {
+        cout << "The stack is empty" << endl;
+        return op;
+    }
+    else
+    {
+        cout << "The element at the top is: " << topT -> StackDataT -> getData();
+        op = (tree *) topT;
+        //op = op -> getData();
+        //op -> setLeft(op -> getLeft());
+        //op -> setRight(op -> getRight());
+        return op;
+    }
+    
+}
+
+//Test function for debugging
 tree* createData()
 {
     tree* a = new tree("a");
@@ -386,4 +480,48 @@ tree* createData()
     e -> setRight(f);
     
     return a;
+}
+
+//Build a binary expression tree from the postfix queue
+tree* buildTree()
+{
+    // build tree
+    Queue* position;
+    position = front;
+    
+    //Traverse the queue left to right
+    while (position != rear->QueueLink)
+    {
+        char treeData = position -> QueueData;
+        //if it is  an operand, push it in the stack
+        if (isalpha(treeData))
+        {
+            char* dataptr = &treeData;
+            tree* operand = new tree(dataptr);
+            StackPushT(operand);
+            cout << "Pushed" << endl;
+        }
+        //if it is an operator, then assign the left and right to be the two top stack nodes
+        else if (treeData == '+' || treeData == '-' || treeData == '/' || treeData == '*' || treeData == '^')
+        {
+            // tree* r = popT();
+            // tree* l = popT();
+            tree* t = new tree(&treeData);
+            tree* right = peekT();
+            popT();
+            tree* left = peekT();
+            popT();
+            cout << "Pushed" << endl;
+            // Right branch = pop
+            t -> setRight(right);
+            // left branch = pop
+            t -> setLeft(left);
+            StackPushT(t);
+        }
+        //Move forward in the queue
+        position = position -> QueueLink;
+    }
+    
+    //tree* root = new tree(peekT());
+    return (tree *) topT;
 }
